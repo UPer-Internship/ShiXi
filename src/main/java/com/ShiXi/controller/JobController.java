f4de8e7776085ce178eccbd54291b979824c5538
cloog <395773508@qq.com>
2025-05-19 02:05:53 +0800

    添加了模糊匹配查询岗位的功能

--- src/main/java/com/ShiXi/controller/JobController.java
+++ src/main/java/com/ShiXi/controller/JobController.java
@@ -1,13 +1,10 @@
 package com.ShiXi.controller;
 
+import com.ShiXi.dto.JobPageQueryDTO;
 import com.ShiXi.dto.Result;
-import com.ShiXi.entity.Job;
 import com.ShiXi.service.JobService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -15,5 +12,24 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/job")
 public class JobController {
+    @Resource
+    private JobService jobService;
 
+    /**
+     * 分页且按条件查询岗位
+     * @return 分页查询结果
+     */
+    @GetMapping("/pageQuery")
+    public Result pageQuery(@RequestBody JobPageQueryDTO jobPageQueryDTO){
+        return jobService.pageQuery(jobPageQueryDTO);
+    }
+
+    /**
+     * 根据岗位id返回
+     * @return 单个岗位信息
+     */
+    @GetMapping("/queryById")
+    public Result queryById(@RequestParam("id") Long id){
+        return jobService.queryById(id);
+    }
 }
