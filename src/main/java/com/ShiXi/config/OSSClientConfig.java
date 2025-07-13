3d7eb7c8b08518089d76ea81a671f7cab63c0c28
1183430975@qq.com <1183430975@qq.com>
2025-07-13 18:44:45 +0800

    新增ossClientConfig以及对应的yaml配置

---
+++ src/main/java/com/ShiXi/config/OSSClientConfig.java
@@ -0,0 +1,30 @@
+package com.ShiXi.config;
+
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OSSClientConfig {
+    // 从配置文件读取 OSS 信息
+
+    @Value("${spring.oss.endpoint")
+    private String endpoint;
+
+    @Value("${spring.oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${spring.oss.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${spring.oss.bucketName}")
+    private String bucketName;
+
+    @Bean
+    public OSS ossClient() {
+        return new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+    }
+}
