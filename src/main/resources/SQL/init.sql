d0a1e97b5e2fe0639274b4a948343a6939b9a554
cloog <395773508@qq.com>
2025-07-17 00:37:04 +0800

    解决冲突

--- src/main/resources/SQL/init.sql
+++ src/main/resources/SQL/init.sql
@@ -118,28 +118,3 @@ CREATE TABLE contact (
      FOREIGN KEY (user_id) REFERENCES user(id),
      FOREIGN KEY (contact_user_id) REFERENCES user(id)
 ) ENGINE = InnoDB;
-
--- 消息表
-CREATE TABLE message (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  sender_id BIGINT NOT NULL,
-  receiver_id BIGINT NOT NULL,
-  content TEXT NOT NULL,
-  send_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (sender_id) REFERENCES user(id),
-  FOREIGN KEY (receiver_id) REFERENCES user(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- 联系人表
-CREATE TABLE contact (
-     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-     user_id BIGINT NOT NULL COMMENT '发起者ID',
-     contact_user_id BIGINT NOT NULL COMMENT '被添加的联系人ID',
-     last_contact_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '最后联系时间',
-     is_blocked BOOLEAN DEFAULT FALSE COMMENT '是否屏蔽该联系人',
-     UNIQUE KEY uk_user_contact (user_id, contact_user_id),
-     FOREIGN KEY (user_id) REFERENCES user(id),
-     FOREIGN KEY (contact_user_id) REFERENCES user(id)
-) ENGINE = InnoDB;
-
-SET FOREIGN_KEY_CHECKS = 1;
