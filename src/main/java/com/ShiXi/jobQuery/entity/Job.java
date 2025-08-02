373072fdff397c1f4e268c9fd3626529f314e39a
jibenjj <1183430975@qq.com>
2025-08-02 15:06:16 +0800

    修改包名

--- src/main/java/com/ShiXi/jobQuery/entity/Job.java
+++ src/main/java/com/ShiXi/jobQuery/entity/Job.java
@@ -22,28 +22,20 @@ public class Job implements Serializable {
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
-  //  @TableField("publisher_id")
     private Long publisherId;
     private Long companyId; // 企业ID，逻辑外键
     private String title;
     private Double salaryMin; // 薪水下限
     private Double salaryMax; // 薪水上限
     private String frequency;//4天/周
-  // @TableField("total_time")
     private String totalTime;//实习总时长
     private String onboardTime;//到岗时间（如一周内、一个月内等）
-   // @TableField("enterprise_type")
     private String enterpriseType;//外企 校友企业
     private String publisher;//hr名字
-  //  @TableField("enterprise_name")
     private String enterpriseName;//腾讯 欧莱雅 阿里巴巴
-  //  @TableField("financing_progress")
     private String financingProgress;//A轮融资 D轮 不需要
-   // @TableField("enterprise_scale")
     private String enterpriseScale;//10000人 100人
-   // @TableField("work_location")
     private String workLocation;//珠海 横琴
-    //@TableField("detailed_information")
     private String detailedInformation;//职位的详情信息
     private String category;//UI设计 游戏策划 ...
     private String type;//实习 兼职 课题
