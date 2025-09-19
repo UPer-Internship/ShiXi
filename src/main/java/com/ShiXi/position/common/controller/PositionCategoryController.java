5db60e8393b9b2d9090809381c3fba958c287151
cloog <395773508@qq.com>
2025-09-19 16:11:51 +0800

    修复industry、major、region三个实体类的错误

--- src/main/java/com/ShiXi/position/common/controller/PositionCategoryController.java
+++ src/main/java/com/ShiXi/position/common/controller/PositionCategoryController.java
@@ -1,12 +1,26 @@
 package com.ShiXi.position.common.controller;
 
+import com.ShiXi.common.domin.dto.Result;
+import com.ShiXi.position.common.service.PositionCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/position/category")
 @Tag(name = "岗位类别相关接口")
 public class PositionCategoryController {
 
+    @Resource
+    private PositionCategoryService positionCategoryService;
+
+    @GetMapping("/getJobCategoryList")
+    @Operation(summary = "获取岗位类别列表")
+    public Result getJobCategoryList() {
+        return positionCategoryService.getJobCategoryList();
+    }
 }
