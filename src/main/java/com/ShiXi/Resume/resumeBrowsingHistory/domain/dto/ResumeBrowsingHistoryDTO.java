e30189ec8e0e455cded6a37edb775b164ae693e9
cloog <395773508@qq.com>
2025-10-20 14:35:44 +0800

    添加简历浏览记录功能

---
+++ src/main/java/com/ShiXi/Resume/resumeBrowsingHistory/domain/dto/ResumeBrowsingHistoryDTO.java
@@ -0,0 +1,15 @@
+package com.ShiXi.Resume.resumeBrowsingHistory.domain.dto;
+
+import lombok.Data;
+
+/**
+ * 简历浏览记录请求DTO
+ */
+@Data
+public class ResumeBrowsingHistoryDTO {
+    
+    /**
+     * 简历ID
+     */
+    private Long resumeId;
+}
